9409439ad6eda6f2e80eb9e8080498c02c88a92c
Thierry <varelathierry@gmail.com>
2022-10-23 01:28:26 -0300

    RE: 6 sem filtros repetidos

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,23 +1,35 @@
-import { useContext, useState } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import context from '../context/STARWARSContext';
 
 function Header() {
+  const [arrayOptions, setArrayOptions] = useState(['population',
+    'orbital_period', 'diameter', 'rotation_period', 'surface_water']);
   const [optionFilter, setOptionFilter] = useState('population');
   const [size, setSize] = useState('maior que');
   const [number, setNumber] = useState('0');
-
   const { setfilterName, setFilterOn, setFormFilter, formFilter } = useContext(context);
 
-  const sendData = () => {
+  useEffect(() => {
+    setOptionFilter(arrayOptions[0]);
+  }, [arrayOptions]);
+
+  const sendData = (option) => {
     const filter = {
       option: optionFilter,
       size,
       number,
     };
+    const newFilter = arrayOptions.filter((e) => e !== option);
+    setArrayOptions(newFilter);
     setFormFilter([...formFilter, filter]);
     setFilterOn(true);
   };
 
+  const handleDelete = (form) => {
+    const newFilter = formFilter.filter((inputFilter) => inputFilter !== form);
+    setFormFilter(newFilter);
+    setFilterOn(false);
+  };
   return (
     <header>
       <h1>Planets</h1>
@@ -35,11 +47,11 @@ function Header() {
           data-testid="column-filter"
           onChange={ ({ target }) => setOptionFilter(target.value) }
         >
-          <option value="population">population</option>
-          <option value="orbital_period">orbital_period</option>
-          <option value="diameter">diameter</option>
-          <option value="rotation_period">rotation_period</option>
-          <option value="surface_water">surface_water</option>
+          {
+            arrayOptions.map((e, i) => (
+              <option key={ i } value={ e }>{e}</option>
+            ))
+          }
         </select>
         <select
           data-testid="comparison-filter"
@@ -58,12 +70,18 @@ function Header() {
         <button
           type="button"
           data-testid="button-filter"
-          onClick={ () => sendData() }
+          onClick={ () => sendData(optionFilter) }
         >
           Filtrar
 
         </button>
       </form>
+      { formFilter.map((form, i) => (
+        <div key={ i }>
+          <p>{`${form.option} ${form.size} ${form.number}`}</p>
+          <button type="button" onClick={ () => handleDelete(form) }>Delete</button>
+        </div>
+      ))}
     </header>
   );
 }
